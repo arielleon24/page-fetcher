408d07413d175c36e120f68da01a14982d1d9745
Ariel Leon <arielleon@MacBook-Pro.local>
2020-10-04 00:56:39 -0400

    fetcher.js functional

---
+++ fetcher.js
@@ -0,0 +1,23 @@
+const { fstat } = require("fs");
+const request = require("request");
+const fs = require("fs");
+const urlToUse = process.argv[2]
+const pathToUse = process.argv[3]
+
+request(urlToUse, (error, response, body) => {
+  // docs request for format. 
+  if (error || response.statusCode !== 200) {
+    console.log(`${error}!`)
+  } else {
+
+
+    fs.writeFile(pathToUse, body, () => {
+
+
+      console.log(` Dowloaded and saved ${urlToUse} (${body.length}bytes) to ${pathToUse}`)
+
+    })
+
+  }
+})  
+
